03e9d89774afc28c48e83a8c687042a5ae4a2782
Denton Elliott <dendo74@hotmail.com>
2021-02-26 23:22:35 -0500

    add seed and schema and resolvers

--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,43 @@
 const schema = `
-
-
-
 """
 A hello world Query
 """
 type Query {
-     hello: String!
+   hello: String!
 }
 
-`
+type ListingType {
+   name: String
+}
+   
+type ListingActivities {
+   name: String
+}
+   
+type Guide {
+   Name: String
+   Bio: String
+   Avatar: String
+}
+   
+type Listing {
+   listingId: String
+   coverPhoto: String
+   listingName: String
+   listingDescription: String
+   listingType: [ListingType]
+   listingLocation: String
+   listingActivities: [ListingActivities]
+   specialType: String
+   specialAmount: Int
+   rating: Int
+   guide: Guide
+   price: String
+   numberOfDays: Int
+}
 
-export {schema}
+type Query {
+   getAllListings: [Listing]
+}
+`
+export { schema }
